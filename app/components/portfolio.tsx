7f980dfcb37ec7871a909e85a3e9379f73908b72
Gitmazter <philip.andelic@gmail.com>
2025-11-23 16:51:54 +0700

    update portfolio styling

--- app/components/portfolio.tsx
+++ app/components/portfolio.tsx
@@ -9,50 +9,50 @@ interface PortfolioProject {
 
 const portfolioProjects:PortfolioProject[] = [
     {
-        title:"Token Terminal Deluxe (Coming Soon)",
+        title:"Token Terminal Deluxe",
         description:"shhhh",
         linkUrl:"",
         imageUrl:""
     },
     {
         title:"ISC Homepage",
-        description:"",
+        description:"A medium length description will go here to explain the project at a glance",
         linkUrl:"https://isc.money/",
         imageUrl:""
     },
     {
         title:"ISC Dashboard",
-        description:"",
+        description:"A medium length description will go here to explain the project at a glance",
         linkUrl:"https://dashboard.isc.money/",
         imageUrl:""
     },
     {
         title:"ISC Points System (Prospera)",
-        description:"",
+        description:"A medium length description will go here to explain the project at a glance",
         linkUrl:"",
         imageUrl:""
     },
     {
         title:"Meteora PnL Dashboard",
-        description:"",
+        description:"A medium length description will go here to explain the project at a glance",
         linkUrl:"",
         imageUrl:""
     },
     {
         title:"CsForge",
-        description:"",
+        description:"A medium length description will go here to explain the project at a glance",
         linkUrl:"https://github.com/Gitmazter/csForge",
         imageUrl:""
     },
     {
         title:"Spritely",
-        description:"pixel art NFT draw and mint ui",
+        description:"A medium length description will go here to explain the project at a glance",
         linkUrl:"https://github.com/Gitmazter/spritely-ts",
         imageUrl:""
     },
     {
-        title:"SGBuilders Bill Splitter",
-        description:"",
+        title:"Spliscwise",
+        description:"A medium length description will go here to explain the project at a glance",
         linkUrl:"https://dashboard.isc.money/#/sgbuilders",
         imageUrl:""
     },
@@ -65,31 +65,21 @@ export default () => {
     return (
         <section className="w-[100vw] max-w-5xl flex flex-col mt-3">
             <h1 className="text-3xl margin-0 font-spectral">Portfolio</h1>
-            <div className="grid gap-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    Token Terminal Deluxe (Coming Soon)
-                </div>
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    ISC Homepage
-                </div>
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    ISC Dashboard
-                </div>
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    ISC Points System
-                </div>
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    Meteora Dashboard
-                </div>
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    CSForge
-                </div>
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    Pixel art nft Project
-                </div>
-                <div className=" h-[200px] bg-[#505050] text-black">
-                    SGBuilders Bill Splitter
-                </div>
+            <div className="grid gap-4 md:grid-cols-2 sm:grid-cols-1">
+                {portfolioProjects.map((e, index) => {
+                    return (
+                        <div key={index} className="grid grid-cols-2 h-[200px] bg-[#505050] text-black p-2 rounded-lg">   
+                            <img className="h-full bg-white" src={""} alt="noimg"/>
+                            <div className="flex flex-col justify-between">
+                                <div className="pl-3">
+                                    <h3 className="nowrap text-lg font-bold text-white">{e.title}</h3>
+                                    <p className="text-[#ffffffa0]">{e.description}</p>
+                                </div>
+                                <a href={e.linkUrl} target="new" className="bg-white px-2 py-1 rounded-xl w-max self-end">Visit</a>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </section>
     )
